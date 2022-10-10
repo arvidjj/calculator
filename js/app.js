48bc8733b671f91abbd0579a3ff07756c59aa6ef
arvidjj <andy.oj14@gmail.com>
2022-10-10 13:08:28 -0300

    Index, primitive app

---
+++ js/app.js
@@ -0,0 +1,35 @@
+
+
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a , b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a*b;
+}
+
+function divide(a, b) {
+    return a/b;
+}
+
+function operate(a, b, operator) {
+    switch (operator) {
+        case '+':
+            return add(a, b);
+        case '-':
+            return subtract(a, b);
+        case '*':
+            return multiply(a, b);
+        case '/':
+            return divide(a, b);
+    }
+}
+
+calcInput = document.querySelector('#calcInput');
+calcResult = document.querySelector('#calcResult');
+
+numberButtons = document.querySelectorAll('.button-grid .btn');
